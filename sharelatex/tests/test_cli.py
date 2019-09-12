070ec2a782a33b940621d6fb625c085275d6e8d6
msimonin <matthieu.simonin@inria.fr>
2019-09-12 18:00:48 +0200

    Tests: syntactic sugar

--- sharelatex/tests/test_cli.py
+++ sharelatex/tests/test_cli.py
@@ -6,7 +6,6 @@ import tempfile
 import unittest
 
 
-
 BASE_URL = os.environ.get("CI_BASE_URL")
 USERNAME = os.environ.get("CI_USERNAME")
 PASSWORD = os.environ.get("CI_PASSWORD")
@@ -14,40 +13,61 @@ PASSWORD = os.environ.get("CI_PASSWORD")
 
 def log(f):
     def wrapped(*args, **kwargs):
-        print("-"*60)
+        print("-" * 60)
         print("{:^60}".format(f.__name__.upper()))
-        print("-"*60)
+        print("-" * 60)
         return f(*args, **kwargs)
+
     return wrapped
+
+class Project():
+    
+    def __init__(self, client, project_id, fs_path):
+        self.client = client
+        self.project_id = project_id
+        self.fs_path = fs_path
+        self.url = f"{BASE_URL}/project/{project_id}"
+
 
 @contextmanager
 def project(project_name):
+    """A convenient contextmanager to create a temporary project on sharelatex."""
     client = SyncClient(base_url=BASE_URL, username=USERNAME, password=PASSWORD)
     with tempfile.TemporaryDirectory() as temp_path:
         os.chdir(temp_path)
         r = client.new(project_name)
         project_id = r["project_id"]
-        path = os.path.join(temp_path, project_id)
+        fs_path = os.path.join(temp_path, project_id)
         # TODO(msimonin) yield the repo object also
-        yield (client, path, project_id)
+        yield Project(client, project_id, fs_path)
         client.delete(project_id, forever=True)
+
+
+def new_project(f):
+    """A convenient decorator to launch a function in the context of a new project."""
+
+    def wrapped(*args, **kwargs):
+        with project(f.__name__) as p:
+            return f(*args, p, **kwargs)
+
+    return wrapped
+
 
 class TestCli(unittest.TestCase):
 
-    def test_clone(self):
-        with project("clone") as (_, _, project_id):
-            url = f"{BASE_URL}/project/{project_id}"
-            check_call(
-                f"git slatex clone {url} --username={USERNAME} --password={PASSWORD} --save-password",
-                shell=True,
-            )
+    @new_project
+    def test_clone(self, project):
+        check_call(
+            f"git slatex clone {project.url} --username={USERNAME} --password={PASSWORD} --save-password",
+            shell=True,
+        )
 
-    def test_clone_and_push(self):
-        with project("clone_and_pull") as (_, path, project_id):
-            url = f"{BASE_URL}/project/{project_id}"
-            check_call(
-                f"git slatex clone {url} --username={USERNAME} --password={PASSWORD} --save-password",
-                shell=True,
-            )
-            os.chdir(path)
-            check_call("git slatex pull", shell=True)
+    @new_project
+    def test_clone_and_push(self, project):
+        check_call(
+            f"git slatex clone {project.url} --username={USERNAME} --password={PASSWORD} --save-password",
+            shell=True,
+        )
+        os.chdir(project.fs_path)
+        check_call("git slatex pull", shell=True)
+
