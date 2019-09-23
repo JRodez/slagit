4bb962e2d3afadccc8a6ec88ea0c26bc8f32db26
msimonin <matthieu.simonin@inria.fr>
2019-09-23 12:00:43 +0200

    Add client version in requests
    Fix #16

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -
+import os
 from setuptools import setup
 
-setup()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'sharelatex', '__version__.py')) as f:
+    exec(f.read(), about)
+
+setup(version=about["__version__"])
